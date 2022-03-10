2abb1fec1288235105cb6ec6a53156c2b420fe20
HAMZA PALA <hamzapala48@gmail.com>
2022-03-11 01:40:13 +0300

    done

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-// const hour = document.querySelector(".hour");
-// const minute = document.querySelector(".minute");
-// const second = document.querySelector(".second");
-// const mood = document.querySelector(".mood");
-// function setTime() {
-//     let realTime = new Date();
-//     let realHour = realTime.getHours();
-//     let realMinute = realTime.getMinutes();
-//     let realSecond = realTime.getSeconds();
-//     if (realHour >= 12) {
-//         realHour -= 12;
-//         mood.innerText = "PM"
-//     } else{
-//         mood.innerText = "AM"
-//     }
-//     hour.innerHTML = realHour.toString().padStart(2, "0") + ":";
-//     minute.innerHTML = realMinute.toString().padStart(2, "0") + ":";
-//     second.innerHTML = realSecond.toString().padStart(2, "0") + "&nbsp;";
-// }
-// setInterval(setTime, 1000);
 let realTime = new Date();
 let realSecond = (realTime.getMilliseconds()/10).toFixed(0);
-console.log(realSecond);
-
-
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 const mili = document.querySelector(".mili");
@@ -30,12 +7,10 @@ const start = document.querySelector(".start");
 const pause = document.querySelector(".pause");
 const stop = document.querySelector(".stop");
 let myMinute = 0
-
 stop.addEventListener("click", ()=>{
     minute.innerHTML = "00";
     second.innerHTML = "00";
-    mili.innerHTML = "00";
-    
+    mili.innerHTML = "00";   
 })
 start.addEventListener("click", ()=>{
     let myInterval = setInterval(() => {
